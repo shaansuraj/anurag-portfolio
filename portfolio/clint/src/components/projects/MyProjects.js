16e30703af6312d59bcc5a4c6e180f6f1c8726fa
Anurag Kumar <93537069+anuragkmr45@users.noreply.github.com>
2022-09-24 19:07:44 +0530

    1st commit
    base of portfolio

---
+++ portfolio/clint/src/components/projects/MyProjects.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './MyProject.css'
+
+function MyProjects() {
+    return (
+        <>
+        <section className='bgimg py-5'>
+            <div className='text-center'>
+                <h4 style={{color: '#cbb26a'}}>CASE STUDIES</h4>
+                <h2>Project Work</h2>
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default MyProjects
