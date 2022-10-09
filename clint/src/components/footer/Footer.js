86efff4c2989fce91d07100791b87711ea6827cc
Anurag Kumar <93537069+anuragkmr45@users.noreply.github.com>
2022-10-09 18:58:15 +0530

    Add files via upload

--- clint/src/components/footer/Footer.js
+++ clint/src/components/footer/Footer.js
@@ -20,11 +20,11 @@ function Footer() {
                     </div>
                 </div>
                 <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted"><BsInstagram fontSize='2rem' /></a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted"><BsLinkedin fontSize='2rem' /></a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted"><BsGithub fontSize='2rem' /></a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted"><BsTwitter fontSize='2rem' /></a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted"><FaDev fontSize='2rem' /></a></li>
+                    <li className="nav-item"><a href="https://www.instagram.com/anuragkmr_45/" target='_blank'  rel="noreferrer" className="nav-link px-2 text-muted"><BsInstagram fontSize='2rem' /></a></li>
+                    <li className="nav-item"><a href="https://www.linkedin.com/in/anurag-kumar-37aab221a/" target='_blank'  rel="noreferrer" className="nav-link px-2 text-muted"><BsLinkedin fontSize='2rem' /></a></li>
+                    <li className="nav-item"><a href="https://github.com/anuragkmr45" target='_blank'  rel="noreferrer" className="nav-link px-2 text-muted"><BsGithub fontSize='2rem' /></a></li>
+                    <li className="nav-item"><a href="https://twitter.com/anuragkmr45" target='_blank'  rel="noreferrer" className="nav-link px-2 text-muted"><BsTwitter fontSize='2rem' /></a></li>
+                    <li className="nav-item"><a href="https://dev.to/anuragkmr45" target='_blank'  rel="noreferrer" className="nav-link px-2 text-muted"><FaDev fontSize='2rem' /></a></li>
                 </ul>
                 <p className="text-center text-muted">Designed & Developed by Anurag Kumar </p>
                 <p className="text-center text-muted">©  Copyright 2022, ALL RIGHTS RESERVED</p>
